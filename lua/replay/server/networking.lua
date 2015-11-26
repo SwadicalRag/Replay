7c78005c8949b20bae996b5209124423ceec4c64
Swad <SwadicalRag@users.noreply.github.com>
2015-11-27 02:12:20 +1100

    comments! (and change an error() to Replay:logError())

--- lua/replay/server/networking.lua
+++ lua/replay/server/networking.lua
@@ -4,14 +4,14 @@ net.Receive("replay_setManipulating",function(len,ply)
     local status = net.ReadBool()
     if ply.recorderObject then
         if status then
-            ply.recorderObject:startManipulating()
+            ply.recorderObject:startManipulating()-- and stop recording
         else
             local targetFrame,frameID = ply.recorderObject:getFrameFromCommandNumber(net.ReadInt(24))
             if targetFrame then
                 ply.recorderObject:jumpToAbsoluteFrame(frameID)
                 ply.recorderObject:stopManipulating()
             else
-                error("Unreasonable replay request from "..ply:Nick())
+                Replay:logError("Unreasonable replay request from "..ply:Nick())-- should never happen
             end
         end
     end
