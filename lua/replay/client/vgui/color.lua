7a084b5e7446bdc00f7e6243a0b24eb1aef3598f
Swad <SwadicalRag@users.noreply.github.com>
2015-11-27 04:15:33 +1100

    oops haha

--- lua/replay/client/vgui/color.lua
+++ lua/replay/client/vgui/color.lua
@@ -12,8 +12,10 @@ local colorData = {
 
 hook.Add("RenderScreenspaceEffects","Replay_GreyEffect",function()
     if(Replay:mStatus()) then
-        colorData["pp_colour_colour"] = math.max(0,colorData["pp_colour_colour"] - FrameTime())
-    elseif(colorData["pp_colour_colour"] ~= 1) then
-        colorData["pp_colour_colour"] = math.min(1,colorData["pp_colour_colour"] + FrameTime())
+        colorData["$pp_colour_colour"] = math.max(0,colorData["$pp_colour_colour"] - FrameTime()*2)
+        DrawColorModify(colorData)
+    elseif(colorData["$pp_colour_colour"] ~= 1) then
+        colorData["$pp_colour_colour"] = math.min(1,colorData["$pp_colour_colour"] + FrameTime()*5)
+        DrawColorModify(colorData)
     end
 end)
