7875d21cad261da81211117129dc9f2ea710942d
Swad <SwadicalRag@users.noreply.github.com>
2015-11-27 02:47:17 +1100

    Finalise calcview override

--- lua/replay/client/vgui/view.lua
+++ lua/replay/client/vgui/view.lua
@@ -1,7 +1,10 @@
 hook.Add("CalcView","ReplaySmoothView",function(ply,_,__,fov,___,____)
     if(Replay:mStatus()) then
+        local frame = Replay:currentFrame()
         return {
-
+            origin = frame.eyePos,
+            angles = frame.eyeAngles,
+            fov = fov
         }
     end
 end)
